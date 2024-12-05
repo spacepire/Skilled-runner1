812f06b1af4100c3e4ccb5d32a90dfaeb5f617cd
Ömer Faruk <85077740+spacepire@users.noreply.github.com>
2024-12-06 01:34:04 +0300

    çevre değikenkerşi eklendi

--- Car Racing Runner/Assets/Scripts/InstantiateManager.cs
+++ Car Racing Runner/Assets/Scripts/InstantiateManager.cs
@@ -2,26 +2,34 @@ using DG.Tweening;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class InstantiateManager : MonoBehaviour
 {
     [SerializeField] Transform player;
     [Header("Objects")]
     [SerializeField] GameObject roads;
+    [SerializeField] GameObject[] splines;
 
     [SerializeField] GameObject[] coinOrBuild;
 
     private float roadLenght = 681.5f;
     private float roadM = 30;
 
+    public float terLenght = 681.5f;
+
+    [SerializeField] float denemef = 1f;
+
 
     private float coinLenght = 50;
 
     private void Update()
     {
+
         if (player != null)
         {
-            EnviromentsInstantiate();
+                EnviromentsInstantiate();
+
             CollettibleInstantiate();
         }
     }
@@ -29,10 +37,20 @@ public class InstantiateManager : MonoBehaviour
 
     private void EnviromentsInstantiate()
     {
-        if (player.position.z >= roadLenght / 5)
+        if (player.position.z >= roadLenght / 2)
         {
             Instantiate(roads, transform.position + new Vector3(0, 0, roadLenght), Quaternion.identity);
             roadLenght += roadM;
+        }
+        if (player.position.z >= terLenght / 2)
+        {
+            //splines
+            Instantiate(splines[Random.Range(0, splines.Length)], transform.position + new Vector3(21f, 0, roadLenght), Quaternion.identity);
+            terLenght += denemef;
+            terLenght -= denemef;
+            
+            Instantiate(splines[Random.Range(0, splines.Length)], transform.position + new Vector3(-21f, 0, roadLenght), Quaternion.identity);
+            terLenght += denemef;
         }
     }
     
